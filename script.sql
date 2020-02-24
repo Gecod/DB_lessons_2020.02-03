74273bf2e0bae6a847a5eed19a22e075ddd66398
Rene-bool <56275019+Rene-bool@users.noreply.github.com>
2020-02-24 19:02:18 +0400

    Задание 2 урока (основное)

---
+++ script.sql
@@ -0,0 +1,14 @@
+DROP DATABASE IF EXISTS example;
+CREATE DATABASE example;
+USE example;
+DROP TABLE IF EXISTS users;
+CREATE TABLE users (
+ id INT UNSIGNED,
+ name VARCHAR(255)
+);
+
+DROP DATABASE IF EXISTS sample;
+CREATE DATABASE sample;
+
+mysqldump -u root -p example > example.sql
+mysql -u root -p sample < example.sql
